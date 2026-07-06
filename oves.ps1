90d1f428f6cfea23c3ae7b40b44fe4356a1a26b4
ovesmulla <oves.mulla@gmail.com>
2026-07-06 23:40:12 +0530

    update shellscripting

---
+++ oves.ps1
@@ -0,0 +1,41 @@
+
+
+# $Oves = "Ovesmulla1"
+# $satya = "SatyaPrakesh"
+# $prashant = "PrashnatSharma"
+# $ssk = "sukhbirKhalsa"
+
+# $Oves = Read-Host
+# $satya = Read-Host
+# $prashant = Read-Host
+# $ssk = Read-Host
+# $ING = Read-Host
+
+# Write-Host $Oves
+# Write-Host $satya
+# Write-Host $prashant
+# Write-Host $ssk
+# Write-Host $ING
+
+# $x = Read-Host
+# $y = Read-Host
+
+# $listofitem = "noodles","briyani","vadapav","sandwich"
+
+# foreach ($item in $listofitem) {
+#     Write-Host $item
+# }
+
+# $listofitem = 1,2,3,4,5,6,7,8,9,10
+
+# foreach ($item in $listofitem) {
+#     ($item)*99
+# }
+
+# $listofitem = "noodles","briyani","vadapav","sandwich"
+
+# foreach ($item in $listofitem) {
+#     Write-Host "oves"-($item)- "Prashant"- "SSK"
+# }
+
+
